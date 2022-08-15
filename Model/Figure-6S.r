a62d5e59d145c4dac74ff56786b5b8c7b32ebeb3
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2022-08-15 18:52:41 +0200

    Update Figure-6S.r

--- Model/Figure-6S.r
+++ Model/Figure-6S.r
@@ -9,13 +9,21 @@ del<-c("kd1","kd2","kd6","kd7","kd8","kd9","kdL","kd10","kd13")
 df = df[,!(names(df) %in% del)]
 dat<-melt(df)
 ################################################
-p<-ggplot(dat, aes(x=value, color=variable, fill=variable)) +
+ggplot(dat, aes(x=value, color=variable, fill=variable)) +
 geom_histogram(alpha=0.5, binwidth = 0.1) +
-theme_ipsum() +
-theme(legend.position="none",panel.spacing = unit(0.1, "lines"),strip.text.x = element_text(size = 8))+
-xlab("") +
-ylab("Slected parameter set") +
-facet_wrap(~variable)
-p+ geom_density(aes(x = value)) + facet_wrap(~variable,scales = "free")
-#p+geom_density(col = "#1b98e0", size = 1)
+geom_density(alpha=.2)+
+     #scale_fill_viridis(discrete=TRUE) +
+    #scale_color_viridis(discrete=TRUE) +
+    #geom_histogram(alpha=0.2, binwidth = 0.1) +
+  theme_ipsum() +
+  theme(
+  legend.position="none",
+  panel.spacing = unit(0.1, "lines"),
+  strip.text.x = element_text(size = 8)
+  )+
+   xlab("") +
+   ylab("Slected parameter set") +
+  facet_wrap(~variable)
+
+
 
